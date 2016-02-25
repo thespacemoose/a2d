b8277ef3b171757fac973426d12b15ff399c7105
thespacemoose <daniel.oakins@gmail.com>
2016-02-25 16:53:18 -0500

    newest

--- js/app.js
+++ js/app.js
@@ -11,6 +11,16 @@ $( document ).ready(function() {
 		$("#conAnswerBoxes").toggleClass("answerOpen");
 	});
 
+	$("#startNew").click(function(){
+		console.log("trying to open");
+		$(".topicTitleForm").toggleClass("formOpen");
+	});
+
+
+	// CREATE A REFERENCE TO FIREBASE
+	var argueRef = new Firebase('https://blistering-heat-1060.firebaseio.com/');
+	var titleRef = new Firebase('https://a2dname.firebaseio.com')
+
 	// CREATE A REFERENCE TO FIREBASE
 	var argueRef = new Firebase('https://blistering-heat-1060.firebaseio.com/');
 
@@ -26,10 +36,26 @@ $( document ).ready(function() {
   	var conArguementTitleList = $('#conPastArgumentsTitle');
   	var conArguementContentList = $('#conPastArguments');
 
+	$("#topicTitleButton").click(function(e){
+		  	e.preventDefault();
+		  	console.log("user trying to change topic")
+		  	// return confirm("DO YOU REALLY WANT TO DO THIS? This will erase the current debate and all arguments associated with it!!!");
+		  	$("h3").empty();
+		   	$(".argumentElement").remove();
+		  	var newTopic = $("#topicTitleInputArea").val();
+		  	console.log(newTopic);
+		  	$(".argument").remove();
+		  	$("#topicTitleInputArea").val("");
+		  	$(".topicTitleForm").removeClass("formOpen");
+
+		  	titleRef.set(null);
+		  	titleRef.push({debateTitle: newTopic});
+		  	argueRef.set(null);
+		});
 
 
 
-
+   // LISTEN FOR KEYPRESS EVENT & SAVE DATA TO FIREBASE AND EMPTY FIELD
 
    // LISTEN FOR KEYPRESS EVENT
 	$("#proAnswerBoxes").submit(function(e){
@@ -66,8 +92,7 @@ $( document ).ready(function() {
 	    argueRef.push({contitle: conUserTitle, conargue: conUserArgument});
 	   	conArguementField.val("");
 	   	conTitleField.val("")
-	});
-
+	});	     
 
       //SAVE DATA TO FIREBASE AND EMPTY FIELD
 	     
@@ -103,73 +128,17 @@ $( document ).ready(function() {
 	    		$(".con").append(argumentElement+conArguementTitleElement+conTitle+closeTitle+conArguementContentElement+conArguement+closeArgumentP+closeArgumentElement);
 
 	    	}
- 	    	
+		});
+		titleRef.on('child_added', function (snapshotTitle) {
+			var dataTitle = snapshotTitle.val();
 
-	    	//CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-
-	   
-	    	// var uniqueArgumentId = 
-	    	// $(".proPastArgumentsTitle").html("TEST CONTENT");
-	    	// $(".proPastArguments").html(proArguement);
-	    	// nameElement.text(username);
-	    	// messageElement.text(message).prepend(nameElement);
+			var databaseTitle = dataTitle.debateTitle;
+			console.log(databaseTitle);
+			$("h3").append(databaseTitle);
+		});	
 
 
-	});
+
+
 });
 	
-
-// when the page is ready
-// call firebase api
-// check firebase for content to fill
-// fill content
-// 
-//  
-// 
-// remove default action of click submit
-// When user clicks submit
-// take content of user input fields and send to firebase
-// check firebase api
-// Pull new content into the arguement and argument title divs
-
-// <!-- CHAT JAVACRIPT -->
-// <script>
-//   // CREATE A REFERENCE TO FIREBASE
-//   var messagesRef = new Firebase('https://blistering-heat-1060.firebaseio.com/');
-
-//   // REGISTER DOM ELEMENTS
-//   var messageField = $('#messageInput');
-//   var nameField = $('#nameInput');
-//   var messageList = $('#example-messages');
-
-//   // LISTEN FOR KEYPRESS EVENT
-//   messageField.keypress(function (e) {
-//     if (e.keyCode == 13) {
-//       //FIELD VALUES
-//       var username = nameField.val();
-//       var message = messageField.val();
-
-//       //SAVE DATA TO FIREBASE AND EMPTY FIELD
-//       messagesRef.push({name:username, text:message});
-//       messageField.val('');
-//     }
-//   });
-//   // Add a callback that is triggered for each chat message.
-//   messagesRef.limitToLast(10).on('child_added', function (snapshot) {
-//     //GET DATA
-//     var data = snapshot.val();
-//     var username = data.name || "anonymous";
-//     var message = data.text;
-
-//     //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
-//     var messageElement = $("<li>");
-//     var nameElement = $("<strong class='example-chat-username'></strong>")
-//     nameElement.text(username);
-//     messageElement.text(message).prepend(nameElement);
-
-//     //ADD MESSAGE
-//     messageList.append(messageElement)
-
-//     //SCROLL TO BOTTOM OF MESSAGE LIST
-//     messageList[0].scrollTop = messageList[0].scrollHeight;
-//   });
